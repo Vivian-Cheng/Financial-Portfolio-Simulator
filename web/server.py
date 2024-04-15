86996b47a3b4dc9d4f6d293db3e6d5567856cdb2
christina010118 <christina010118@outlook.com>
2024-04-15 14:29:25 -0700

    Modify last commit

--- web/server.py
+++ web/server.py
@@ -1,212 +1,144 @@
 import os
 import sys
-from datetime import datetime
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from datetime import datetime, date
 from flask import Flask, request, jsonify
-import mysql.connector
+from real_time_system.config import MONGODB_SERVER_ADDR, TOTAL_BUCKET, DB_NAME
+from real_time_system.db_handler import connect_mongodb, scan_database, ConsistentHash, migrate_data
 
 # Flask app
 app = Flask(__name__)
 
-# Connect to MySQL database
-mysql_conn = mysql.connector.connect(
-    host='localhost',
-    user='dsci551',
-    password='',
-    database='STOCK'
-)
+# Connect to MongoDB
+mongo_client = connect_mongodb(MONGODB_SERVER_ADDR)
+if mongo_client == None:
+    raise Exception(f"Cannot establish connection to MongoDB server at {MONGODB_SERVER_ADDR}")
+num_db = scan_database(mongo_client)
+if num_db == 0:
+    raise Exception("No available database")
+ch = ConsistentHash(num_bucket=TOTAL_BUCKET, num_db=num_db)
 
-# Cursor for executing queries
-mysql_cursor = mysql_conn.cursor()
+def get_realtime_stock_collection(symbol):
+    db_id = ch.get_node(symbol)
+    db_name = DB_NAME + db_id
+    print(db_name)
+    return mongo_client[db_name][symbol]
 
-#HISTORICAL
-@app.route('/stock_data', methods=['GET'])
-def get_stock_data():
-    """
-    Price in a time span from “start” to “end”
-    Arg: Symbol,start,end
-    Return: json
-    """
+"""sample output:
+[
+  {
+    "_id": "AAPL_2024-03-14 18:58:34.912639",
+    "change": 1.87,
+    "current_price": 173,
+    "high_price": 174.3078,
+    "low_price": 172.05,
+    "open_price": 172.94,
+    "percent_change": 1.0927,
+    "prev_close_price": 171.13,
+    "t": 1710446401,
+    "timestamp": "Thu, 14 Mar 2024 18:58:34 GMT"
+  }
+]
+"""
+@app.route('/quote', methods=['GET'])
+def quote():
+    symbol = request.args.get("symbol")
+    collection = get_realtime_stock_collection(symbol)
+    latest_data = collection.find().sort('timestamp', -1).limit(1)
+    return jsonify(list(latest_data))
+
+"""sample output:
+[
+  {
+    "current_price": 173,
+    "timestamp": "Thu, 14 Mar 2024 18:58:34 GMT"
+  },
+  {
+    "current_price": 173,
+    "timestamp": "Thu, 14 Mar 2024 21:58:52 GMT"
+  }
+]
+"""
+@app.route('/quote_chart', methods=['GET'])
+def quote_chart():
     symbol = request.args.get('symbol')
-    start_date = request.args.get('start_date')
-    end_date = request.args.get('end_date')
+    start = request.args.get('start')
+    end = request.args.get('end')
 
-    query = f"SELECT symbol, hdate, o, h, l, c FROM stockhistory WHERE symbol = %s AND hdate BETWEEN %s AND %s"
-    mysql_cursor.execute(query,(symbol, start_date, end_date))
-    data = mysql_cursor.fetchall()
+    format_start = datetime.combine(date.today(), datetime.strptime(start, '%H:%M:%S').time())
+    format_end = datetime.combine(date.today(), datetime.strptime(end, '%H:%M:%S').time())
 
-    if data:
-        return jsonify(data)
-    else:
-        return jsonify({"message": "No data found for the symbol"})
+    query = {
+        'timestamp': {
+            '$gte': format_start,
+            '$lte': format_end
+        }
+    }
 
-
-@app.route('/db_stats', methods=['GET'])
-def get_db_stats():
-    """
-    Database Baisc Stats
-    Arg: table_name, table_schema, engine, 
-         table_rows (# of rows in a table),
-         update_time (last time table was updated)
-    Return: json
-    """
-    #Get summary
-    mysql.cursor.execute("SELECT table_name, table_schema, engine, table_rows, update_time from information_schema.tables")
-    summary_stats=mysql_cursor.fetchall()
+    collection = get_realtime_stock_collection(symbol)
+    data = collection.find(query, {'_id': 0, 'current_price': 1, 'timestamp': 1})
     
-    return jsonify(summary_stats)
+    return jsonify(list(data))
 
-@app.route('/data_availability', methods=['GET'])
-def get_data_availability():
-    """
-    Display data updates
-    Arg: Date, Availability
-    Return: json
-    """
-    mysql.cursor.execute("""SELECT sh.hdate AS Date, CONCAT(count(sh.symbol),'/', s.total_stock_count) AS Availability 
-                         FROM stockhistory sh 
-                         JOIN (SELECT count(*) AS total_stock_count FROM stocks) AS s 
-                         ON 1=1 
-                         GROUP BY sh.hdate""")
-    data_availability=mysql_cursor.fetchall()
-    
-    return jsonify(data_availability)
+@app.route('/realtime/dbstats', methods=['GET'])
+def dbstats():
+    db_list = [name for name in mongo_client.list_database_names() if name.startswith(DB_NAME)]
+    db_stat = {'stats':{}, 'collections':{}}
+    for db_name in db_list:
+        db = mongo_client[db_name]
+        db_stat['stats'][db_name] = db.command('dbStats')
+        db_stat['collections'][db_name] = {"collections": db.list_collection_names()}
+    return jsonify(db_stat)
 
-#PORTFOLIO
-@app.route('/user_verif', methods=['GET'])
-def user_verification():
-    """
-    Verify user log in information
-    Arg: username, password
-    Return: True/False
-    """
-    usrname = request.args.get('username')
-    passwrd = request.args.get('password')
-    query = """
-            SELECT 
-                CASE 
-                    WHEN COUNT(*) > 0 THEN 'true'
-                    ELSE 'false'
-                END AS is_verified
-            FROM 
-                users
-            WHERE 
-                username = %s AND 
-                password = %s
-            """
-    mysql_cursor.execute(query, (usrname, passwrd))
-    result = mysql_cursor.fetchone()[0]
-    
-    return str(bool(result))
+@app.route('/realtime/commands', methods=['GET'])
+def commands():
+    db = request.args.get("db")
+    collection = request.args.get("collection")
+    action = request.args.get("action")
+    query = request.args.get("query")
 
+    if action == "drop":
+        try:
+            result = mongo_client[db][collection].drop()
+            print(result)
+            return jsonify(message="Success!")
+        except Exception as e:
+            print(e)
+            return jsonify(message="Fail!")
+    if action == "count":
+        res = mongo_client[db][collection].count_documents({})
+        return jsonify(res)
+    if action == "remove_expired":
+        format_time = datetime.combine(date.today(), 
+                                       datetime.strptime("00:00:00", '%H:%M:%S').time())
+        res = mongo_client[db][collection].delete_many(
+            {'timestamp': {'$lt': format_time}})
+        return jsonify(res.deleted_count)
+    if action == "drop_all":
+        collections = mongo_client[db].list_collection_names()
+        for c in collections:
+            if c != "dummy":
+                mongo_client[db][c].drop()
+        return jsonify(message="Success!")
+        
+@app.route('/realtime/addnode', methods=['GET'])
+def addnode():
+    new_idx = ch.add_node(str(len(ch.nodes)))
+    old_idx = (new_idx + 1) % len(ch.nodes)
+    new_node = f"{DB_NAME}{ch.nodes[new_idx]}"
+    old_node = f"{DB_NAME}{ch.nodes[old_idx]}"
+    migrate_data(mongo_client=mongo_client, old_node=old_node, ch=ch)
+    return jsonify({"new_db": new_node, "old_db": old_node})
 
-@app.route('/user_registration', methods=['POST'])
-def user_registration():
-    """
-    User registration, insert data if user doesn't exist
-    Arg: username
-    Return: True/False
-    """
-    usrname = request.args.get('username')
-    query = """
-            INSERT IGNORE INTO users (username) 
-            VALUES (%s)
-            """
-    try:
-        mysql_cursor.execute(query, (usrname,))
-        mysql_conn.commit()
-        return True  #If successfully inserted
-    except mysql.connector.Error as err:
-        if err.errno == 1062:  #Duplicate entry error
-            return False  #User already exists
-        else:
-            return 'Error: {}'.format(err)  # Other error
-
-
-@app.route('/transaction', methods=['POST'])
-def get_transaction():
-    """
-    Handles Buy/Sell on Webpage.
-    If user purchase or sell stocks, database will be automatically updated with the changes
-    Return: message
-    """
-    #NEED TO ADJUST CODE IN WEBPAGE TO ENSURE THAT WHEN USER PRESS "BUY" THE VOLUME IS POSITIVE, 
-    #AND WHEN "SELL", THE VOLUME SHOULD BE THE INPUTET VOLUME VALUE*(-1)
-    symbol = request.json.get('symbol')
-    volume = request.json.get('volume')
-    current_username= request.json.get('username') #I DONT KNOW HOW TO GET THIS!!!
-    current_time= datetime.now()
-
-    if not (symbol and volume and current_username):
-        return jsonify({'message': 'Invalid request parameters.'}), 400
-    
-    #Retrieve the user ID corresponding to the provided username
-    mysql_cursor.execute("SELECT id FROM users WHERE username = %s", (current_username,))
-    user_id = mysql_cursor.fetchone()
-    if not user_id:
-        return jsonify({'message': f"User with username {current_username} not found."}), 404
-    user_id = user_id[0]
-
-    
-    # Insert transaction into transactions table
-    if volume > 0:
-        # User is buying
-        mysql_cursor.execute("INSERT INTO transactions (buyerId, symbol, volume, txnTime) VALUES (%s, %s, %s, %s)", 
-                            (user_id, symbol, volume, current_time))
-    elif volume < 0:
-        # User is selling
-        mysql_cursor.execute("INSERT INTO transactions (buyerId, symbol, volume, txnTime) VALUES (%s, %s, %s, %s)", 
-                            (user_id, symbol, -volume, current_time))
-    else:
-        return jsonify({'message': 'Volume must be nonzero.'}), 400
-    mysql_conn.commit()
-
-    # Update inventory table
-    if volume >0:
-        mysql_cursor.execute("UPDATE inventory SET volume = volume + %s WHERE symbol = %s and userId= %s ", (volume, symbol, user_id))
-    else:  #Sell
-        mysql_cursor.execute("UPDATE inventory SET volume = volume - %s WHERE symbol = %s and userId= %s ", (-volume, symbol, user_id))
-    
-    mysql_conn.commit()
-
-    return jsonify({'message': 'Transaction processed successful.'})
-
-
-@app.route('/user_info', methods=['GET'])
-def get_user_info():
-    """
-    Display user info in the Portfolio Page
-    Arg: ID, username
-    Return: json
-    """
-    usrname = request.args.get('username')
-    query = "SELECT * FROM users WHERE username = %s"
-    mysql_cursor.execute(query, (usrname, ))
-    user_info = mysql_cursor.fetchone()
-    
-    return jsonify(user_info)
-
-@app.route('/user_inventory', methods=['GET'])
-def get_user_inventory():
-    """
-    Display current user's list of inventories
-    Arg: symbol, volume
-    Return: json
-    """
-    usrname = request.args.get('username')
-    query = """SELECT symbol, volume 
-                FROM inventory i
-                JOIN users u ON i.userId=u.id
-                WHERE u.username = %s"""
-    mysql_cursor.execute(query, (usrname, ))
-    user_inv = mysql_cursor.fetchall()
-    
-    return jsonify(user_inv)
-
-
+@app.route('/realtime/deletenode', methods=['GET'])
+def deletenode():
+    node_id = len(ch.nodes) - 1
+    if node_id < 0:
+        return jsonify(message="No node to delete")
+    node = f"{DB_NAME}{node_id}"
+    ch.remove_node(str(node_id))
+    migrate_data(mongo_client=mongo_client, old_node=node, ch=ch)
+    return jsonify({"db": node})
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
-
-#username password return True or False
-
-#check if user exists, if not, INS
